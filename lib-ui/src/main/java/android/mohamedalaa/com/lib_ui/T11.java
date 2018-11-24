4f458a88601108a096c4835e86978e6f151a04a1
Mohamed Alaa <mohamed.alaa636@hotmail.com>
2018-11-24 21:43:08 +0200

    1 more test isa.

--- lib-ui/src/main/java/android/mohamedalaa/com/lib_ui/T11.java
+++ lib-ui/src/main/java/android/mohamedalaa/com/lib_ui/T11.java
@@ -1,6 +1,7 @@
 package android.mohamedalaa.com.lib_ui;
 
 import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.AppCompatButton;
 
 /**
  * Created by Mohamed on 11/24/2018.
@@ -10,6 +11,8 @@ public class T11 {
 
     private void m1(){
         DialogFragment h;
+
+        AppCompatButton button;
     }
 
 }
