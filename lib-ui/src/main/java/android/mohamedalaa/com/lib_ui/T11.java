697091ff6a3ecf194a2701a82070b1abe21a0a97
Mohamed Alaa <mohamed.alaa636@hotmail.com>
2018-11-24 20:32:11 +0200

    Adding 27.1.1 support library package to see if another developer using diff version how can we handle the error isa.

---
+++ lib-ui/src/main/java/android/mohamedalaa/com/lib_ui/T11.java
@@ -0,0 +1,15 @@
+package android.mohamedalaa.com.lib_ui;
+
+import android.support.v7.widget.AppCompatTextView;
+
+/**
+ * Created by Mohamed on 11/24/2018.
+ *
+ */
+public class T11 {
+
+    private void m1(){
+        AppCompatTextView textView;
+    }
+
+}
